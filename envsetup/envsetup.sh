a4906cf1389861be9c0bae5b5de8537e62080ba0
Zaw Zaw <mczawzaw12@outlook.com>
2018-09-09 16:44:59 +0630

    envsetup: Add GPL-3.0 license for envsetup.sh file
    Signed-off-by: Zaw Zaw <mczawzaw12@outlook.com>

--- envsetup/envsetup.sh
+++ envsetup/envsetup.sh
@@ -1,3 +1,22 @@
+############################################################################
+#                                                                          #
+#  Copyright(C) 2018, Zaw Zaw                                              #
+#                                                                          #
+#  This file is free and you can distribute it and/or modify               #
+#  it under the terms of the GNU General Public License as published by    #
+#  the Free Software Foundation, either version 3 of the License, or       #
+#  any later version.                                                      #
+#                                                                          #
+#  This file is distributed in the hope that it will be useful,            #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            #
+#  GNU General Public License for more details.                            #
+#                                                                          #
+#  You should have received a copy of the GNU General Public License       #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>    #
+#                                                                          #
+############################################################################
+
 #!/usr/bin/env bash
 # A Script to Setup environment for Android ROM Development
 # For Ubuntu Linux
